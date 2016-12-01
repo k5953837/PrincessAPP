c9ed57cb105803985ef55423fce115144004b5d2
kitb <k5953837@gmail.com>
2016-12-01 18:12:47 +0800

    Added devise gem.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:about]
+
   def home
   end
 
   def about
+    # flash[:notice] = "Welcome Back. My Lovely Candy."
   end
 end
