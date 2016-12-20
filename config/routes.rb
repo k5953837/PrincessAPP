e3fde4ba81176e715b00a91433272168df2b9b0d
kitb <k5953837@gmail.com>
2016-12-21 03:39:03 +0800

    Add Commit model and view

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
 
   root 'pages#home'
 
-  resources :posts
+  resources :posts do
+    resources :comments
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
