2db8759328f104b667ccdb4bc0b567b69e1ef7a7
kitb <k5953837@gmail.com>
2016-12-21 11:17:46 +0800

    Add devise authenticate rule for user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [:index, :show]
   def index
     @post = Post.new
     @posts = Post.all.order('created_at DESC')
